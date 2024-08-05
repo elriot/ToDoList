f054b944812f24bce7cc7a13ff54f1f71789af0b
Soopin Kim <spkim8901@gmail.com>
2024-08-05 16:37:32 -0700

    Add Color Extensions.

---
+++ ToDoList/ToDoList/App/Extensions.swift
@@ -0,0 +1,15 @@
+//
+//  Extensions.swift
+//  ToDoList
+//
+//  Created by SOOPIN KIM on 2024-08-05.
+//
+
+import SwiftUI
+
+extension Color {
+    static let background = Color("MainBackground", bundle: nil)
+    static let dullBlue = Color("Blueberry", bundle: nil)
+    static let lightBlue = Color("DarkSky", bundle: nil)
+    static let lightGray = Color("Silver", bundle: nil)
+}
