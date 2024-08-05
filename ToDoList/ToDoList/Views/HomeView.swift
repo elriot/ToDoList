151e1b7be8ef0eb87a7cb710f8cada5ed9846840
Soopin Kim <spkim8901@gmail.com>
2024-08-05 14:24:51 -0700

    Add basic ListView layout.

--- ToDoList/ToDoList/Views/HomeView.swift
+++ ToDoList/ToDoList/Views/HomeView.swift
@@ -14,11 +14,11 @@ struct HomeView: View {
     var body: some View {
         NavigationStack {
             TabView {
-                Text("Page1")
+                ListView(title: "To Do")
                 
-                Text("page 2")
+                ListView(title: "In Progress")
                 
-                Text("page 3")
+                ListView(title: "Done")
             }
             .tabViewStyle(.page)
             .navigationBarTitleDisplayMode(.inline)
