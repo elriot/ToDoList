732f9eaa5b3f8ed97fb6af3e80c931ef748701f9
Soopin Kim <spkim8901@gmail.com>
2024-08-05 16:14:58 -0700

    Add basic UI to ListItemView.

--- ToDoList/ToDoList/Views/ListItemView.swift
+++ ToDoList/ToDoList/Views/ListItemView.swift
@@ -11,10 +11,29 @@ struct ListItemView: View {
     let item: Item
     let height: CGFloat
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack(spacing: 10) {
+            VStack(alignment: .leading, spacing: 3) {
+                Text(item.title)
+                    .font(.headline)
+                    .lineLimit(1)
+                
+                Text(item.description)
+                    .font(.body)
+                    .foregroundStyle(.secondary)
+                    .lineLimit(2)
+            }
+            
+            Spacer()
+            
+            Image(systemName: "circle")
+                .resizable()
+                .scaledToFit()
+                .foregroundColor(.blue)
+                .frame(maxHeight: height * 0.45)
+        }
     }
 }
 
-#Preview {
-    ListItemView()
+#Preview(traits: .sizeThatFitsLayout) {
+    ListItemView(item: Item(id: "abcd123", authorId: "John Doe", title: "First item", description: "First", startDate: .now, status: .todo, priority: .low), height: 100)
 }
