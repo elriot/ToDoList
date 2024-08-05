83f65be736662f6e13f8dffff2ba0a225ad36450
Soopin Kim <spkim8901@gmail.com>
2024-08-05 16:09:08 -0700

    add ListItemView to project

---
+++ ToDoList/ToDoList/Views/ListItemView.swift
@@ -0,0 +1,20 @@
+//
+//  ListItemView.swift
+//  ToDoList
+//
+//  Created by SOOPIN KIM on 2024-08-05.
+//
+
+import SwiftUI
+
+struct ListItemView: View {
+    let item: Item
+    let height: CGFloat
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ListItemView()
+}
