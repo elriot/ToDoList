48c9758c24c85361ec0fb85f095688934ff5649b
Soopin Kim <spkim8901@gmail.com>
2024-08-07 13:59:29 -0700

    Add signOut to LoginManager

--- ToDoList/ToDoList/App/LoginManager.swift
+++ ToDoList/ToDoList/App/LoginManager.swift
@@ -13,8 +13,17 @@ final class LoginManager {
     // Fetch login related datea to pass to LoginVM
     
     private let db = Firestore.firestore()
+    private var handler: AuthStateDidChangeListenerHandle?
     var currentUser: User?
     
+    init() {
+        setupListener()
+    }
+    
+    deinit {
+        removeListener()
+    }
+
     func signUp(_ fname: String, _ lname: String, _ email: String, _ pw: String) {
         Task {
             do {
@@ -32,33 +41,31 @@ final class LoginManager {
                 print(error)
             }
         }
+    }
+    
+    func signOut() {
+        do {
+            try Auth.auth().signOut()
+            currentUser = nil
+            removeListener()
+            print("successfully signed out user!")
+        } catch {
+            print(error)
+        }
         
-//        Auth.auth().createUser(withEmail: email, password: pw) { [weak self] result, error in
-//             if let error {
-//                print(error)
-//                return
-//            }
-//            
-//            guard let self, let result else { return }
-//            
-//            currentUser = result.user // firebase User
-//            let id = result.user.uid
-//            
-//            Task { [weak self] in
-//                guard let self else { return }
-//                
-//                do {
-//                    try await db.collection("Users").document(id).setData([
-//                        "fname": fname,
-//                        "lname": lname,
-//                        "email": email,
-//                        "id": id
-//                    ])
-//                    print("Document successfully written!")
-//                } catch {
-//                    print("Error writing document: \(error)")
-//                }
-//            }
-//        }
+    }
+    
+    func setupListener() {
+        guard handler == nil else { return }
+        handler = Auth.auth().addStateDidChangeListener { [weak self] auth, user in
+            guard let self else { return }
+            currentUser = auth.currentUser
+        }
+    }
+    
+    func removeListener() {
+        if let h = handler {
+            Auth.auth().removeStateDidChangeListener(h)
+        }
     }
 }
